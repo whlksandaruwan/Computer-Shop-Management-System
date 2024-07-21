8d784acf844109861ed767dd81eb5ef4bc2afdd0
kavindu <whlksandaruwan@students.nsbm.ac.lk>
2024-07-21 19:40:30 +0530

    commit

---
+++ ComputerShopManagementSystem/PAL/FormReport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerShopManagementSystem.PAL
+{
+    public partial class FormReport : Form
+    {
+        internal static FormReport formReport;
+        public DateTime startDate, endDate;
+
+        public FormReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
